cdee4d4d90e55272bf6b6b505c34c7023b8f33f7
CarlosCode502 <carloshl.code21@gmail.com>
2024-01-27 21:20:35 -0600

    Agregando diseño al sistema (el cambio de version funciona correctamente aunque falta ver el debug)
    (Buscar cambios por //#-- 27/01/2024)
    
    1. reporte.ts(Se creó un metodo para calcular el importe además es posible calcularlo a traves de un filtro).
    
    2. reporte.html (Se agregaron nuevas etiquetas para mostrar msj de ayuda con tooltip).
    
    3. reporte.css (Se agrego estilo al icono de ayuda "?" que va a mostrar un tooltip en la tabla reporte).

--- src/app/Components/layout/Pages/reporte/reporte.component.ts
+++ src/app/Components/layout/Pages/reporte/reporte.component.ts
@@ -50,9 +50,11 @@ export const MY_DATA_FORMATS = {
 })
 export class ReporteComponent {
   //Creamos las variables a utilizar 02.41 parte 14
-
   //#-- Para obtener el importe 26/01/2024 22.27 pm
   obtenerImporteVar: number = 0;
+
+  //#-- Se cambio el valor para detectar si existe un filtro 27/01/2024 16.02 pm
+  filterValue: string = '';
 
   //Contiene los campos de busqueda
   formularioFiltro: FormGroup;
@@ -100,8 +102,8 @@ export class ReporteComponent {
     this.formularioFiltro = this.fb.group({
       //Si este campo se mantiene en fecha se búscara por fecha
       //Campos que son requeridos
-      fechaInicio: ['', Validators.required],
-      fechaFin: ['', Validators.required],
+      fechaInicio: ['2024-01-26T00:00:00-06:00', Validators.required],
+      fechaFin: ['2024-01-27T00:00:00-06:00', Validators.required],
     });
   }
 
@@ -109,13 +111,18 @@ export class ReporteComponent {
   //Recibe un evento
   aplicarFiltroTabla(event: Event) {
     //Obtiene el valor del filtro desde de un imput de tipo HTML
-    const filterValue = (event.target as HTMLInputElement).value;
+    this.filterValue = (event.target as HTMLInputElement).value;
     //Aplica los filtros solo a la fuente de datos de las ventas
     //Se usa trim(para eliminar espacios al inicio y fin) además todo el texto sera en minúsculas
-    this.dataTableVentaReporte.filter = filterValue.trim().toLocaleLowerCase();
+    this.dataTableVentaReporte.filter = this.filterValue.trim();
+    //No devolvia el producto ni tipo pago ya que hacia un to lower a los textos
+    // .toLocaleLowerCase();
     // this.datosListaVentas.paginator = this.paginacionTabla;
+
     //#-- Establece el importe a 0 ya que se requiere un filtro 26/01/2024 22.54pm
     this.obtenerImporteVar = 0;
+
+    // console.log(this.dataTableVentaReporte.filter);
   }
 
   //Crear el evento del componente AfterViewInit para la páginacion min 06.17 parte 14
@@ -126,6 +133,8 @@ export class ReporteComponent {
 
   //Método para poder realizar la búsqueda segun un rango especifico min 06.32 parte 14
   buscarVentasReporte() {
+    //#-- Limpiar el área de filtro 27/01/2024 19.32pm
+
     //Utilizando moment para convertir a fecha con formato
     //Primero se obtiene el valor luego se encapsula con moment
     const _fechaInicio = moment(this.formularioFiltro.value.fechaInicio).format(
@@ -157,11 +166,13 @@ export class ReporteComponent {
 
           //Se actualiza los datos de ventareportes
           this.listaVentasReporte = data.value;
+          // console.log('Listado de ventas reporte');
+          // console.log(this.listaVentasReporte);
+
           //Se actualiza la tabla
           this.dataTableVentaReporte.data = data.value;
-
-          //#-- 26/01/2024 22.52pm
-          // this.obtenerImporte();
+          // console.log('Elementos de datatable');
+          // console.log(this.dataTableVentaReporte.data);
         }
         //En caso de no exitoso
         else {
@@ -174,13 +185,97 @@ export class ReporteComponent {
             'No se encontraron datos',
             'Opss!'
           );
+
+          console.log('Entro al else');
         }
       },
-      error: (e) => {},
+      error: (e) => {
+        console.log('Err');
+      },
     });
   }
 
-  //Variables que contienen las fechas
+  //#-- Permite obtener el Importe total del reporte actual 26/01/2024 22.12pm
+  obtenerImporte() {
+    //#-- Verifica si importe ya tiene una cantidad asignada 26/01/2024 18.41pm
+    if (this.obtenerImporteVar > 1) {
+      //#-- Si tiene cambia el valor a 0 (esto evita que se sume el importe total anterior con el nuevo) 26/01/2024 18.43pm
+      this.obtenerImporteVar = 0;
+      // console.log('Se limpio el importe total dentro del método');
+    }
+
+    //FALTA BORRAR AL TENER FILTRO CAMBIAR A TABLA DE FILTRO PARA CALCULAR LOS TOTALES
+
+    //#-- Si no se ha ingresado algun filtro 26/01/2020 16.30pm
+    if (this.filterValue == '') {
+      // console.log(
+      //   'Elementos de listaVentasReporte dentro del método obtenerImporte()'
+      // );
+      // console.log(this.listaVentasReporte);
+
+      //#-- Se hace una iteración por la longitud de listaVentasReporte
+      for (
+        let indexLVR = 0;
+        indexLVR < this.listaVentasReporte.length;
+        indexLVR++
+      ) {
+        //Para poder acceder a los elementos del array de objetos
+        const element = this.listaVentasReporte[indexLVR];
+
+        //Se reliza la suma el valor de oIV + el total actual y así sucesivamente (usado parse float ya que tiene decimales)
+        this.obtenerImporteVar =
+          this.obtenerImporteVar + parseFloat(element.total);
+      }
+    } else {
+      //Obtener el listado devuelto por el filtro
+      // console.log('\n');
+      // console.log('Listado de ventas reporte (Sino)');
+      // console.log(this.listaVentasReporte);
+      // console.log('\n');
+      // console.log('Elementos de datatable (Sino)');
+      // console.log(this.dataTableVentaReporte.data);
+      // console.log('\n');
+      // console.log('Listado de ventas reporte (Sino) con filtro');
+      // console.log(this.listaVentasReporte);
+
+      //#-- Solo devuelve el filtro
+      // console.log('\n');
+      // console.log('Elementos de datatable (Sino) con filtro');
+      // console.log((this.dataTableVentaReporte.filter = this.filterValue));
+
+      // console.log('\n');
+      // console.log('Elementos de datatable (Sino) con filtro');
+      // console.log((this.dataTableVentaReporte.filter = this.filterValue));
+
+      // console.log('Valor de la tabla luego del filtro');
+      // console.log(
+
+      const lstVR = this.listaVentasReporte.filter(
+        (x) =>
+          x.fechaRegistro === this.dataTableVentaReporte.filter ||
+          x.numeroDocumento === this.dataTableVentaReporte.filter ||
+          x.tipoPago === this.dataTableVentaReporte.filter ||
+          x.producto === this.dataTableVentaReporte.filter
+        // x.cantidad.toString() === this.dataTableVentaReporte.filter ||
+        // x.precio.toString() === this.dataTableVentaReporte.filter ||
+
+        // x.total.toString() === this.dataTableVentaReporte.filter ||
+        // x.totalVenta.toString() === this.dataTableVentaReporte.filter
+      );
+
+      console.log(this.dataTableVentaReporte.filter);
+      // const lstVR = this.listaVentasReporte.
+
+      for (let index = 0; index < lstVR.length; index++) {
+        const element = lstVR[index];
+
+        this.obtenerImporteVar =
+          this.obtenerImporteVar + parseFloat(element.total);
+      }
+
+      // );
+    }
+  }
 
   //Método que permitirá exportar un documento de tipo exel min 09.58 parte 14
   exportarExel() {
@@ -222,22 +317,11 @@ export class ReporteComponent {
     // console.log(wb, _fechaInicio, _fechaFin, _HoraActual.toLocaleString());
   }
 
-  //#-- Permite obtener el Importe total del reporte actual 26/01/2024 22.12pm
-  obtenerImporte() {
-    if (this.obtenerImporteVar > 1) {
-      this.obtenerImporteVar = 0;
-    }
+  limpiarCampos() {
+    this.filterValue = '';
 
-    //FALTA BORRAR AL TENER FILTRO CAMBIAR A TABLA DE FILTRO PARA CALCULAR LOS TOTALES
+    this.listaVentasReporte = [];
 
-    for (let index = 0; index < this.listaVentasReporte.length; index++) {
-      const element = this.listaVentasReporte[index];
-
-      console.log(element.total);
-
-      this.obtenerImporteVar =
-        this.obtenerImporteVar + parseFloat(element.total);
-      console.log(this.obtenerImporteVar);
-    }
+    this.dataTableVentaReporte.data = [];
   }
 }
