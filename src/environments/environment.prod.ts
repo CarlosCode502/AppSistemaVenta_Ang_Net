839c190bfbce78fe6d6eadc97323b00c2eaae7ea
CarlosCode502 <carloshl.code21@gmail.com>
2024-01-29 16:51:29 -0600

    contiene error de depuración

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
-    //Configurando el ambiente de producción min 10.58 parte 7
-    production: true,
-    //Contiene la url donde se esta ejecutando nuestra API min 11.15 parte 7
-    //Eliminar texto de las últimas 2 diagonales (ya que va a cambiar)
-    endpoint: "http://localhost:5137/api/"
+  //Configurando el ambiente de producción min 10.58 parte 7
+  production: true,
+  //Contiene la url donde se esta ejecutando nuestra API min 11.15 parte 7
+  //Eliminar texto de las últimas 2 diagonales (ya que va a cambiar)
+  endpoint: 'http://localhost:5137/api/',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
-        //Configurando el ambiente de producción min 11.58 parte 7
-        production: false,
-        //Contiene la url donde se esta ejecutando nuestra API min 12.00 parte 7
-        //Eliminar texto de las últimas 2 diagonales (ya que va a cambiar)
-        endpoint: "http://localhost:5137/api/"
+  //Configurando el ambiente de producción min 11.58 parte 7
+  production: false,
+  //Contiene la url donde se esta ejecutando nuestra API min 12.00 parte 7
+  //Eliminar texto de las últimas 2 diagonales (ya que va a cambiar)
+  endpoint: 'http://localhost:5137/api/',
 };
